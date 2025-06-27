91ad8898bbf3223e36f88d9b8070947248c6f6b4
Wil-dev <mehdididou7@icloud.com>
2025-06-27 23:42:14 +0200

    Ajout de nouvelles polices et mise à jour du style global. Changement du titre et de la description de la métadonnée. Amélioration de la page d'accueil avec un nouveau texte et des classes CSS pour le style.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
 export default function Home() {
   return (
     <div>
-      <h1>Hello World</h1>
+      <h1 className="text-4xl ">Hello World</h1>
+      <p className="text-base">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.</p>
     </div>
   );
 }
